b2296f17be33e727f9ea46de3ffe05354707f70e
hobbyist-programmer-ar <hobbyistprogrammer@outlook.com>
2025-03-22 22:28:37 +0530

    Initial commit

---
+++ src/main/java/com/hobbyistprogrammer/kafka_chat_system/KafkaChatSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hobbyistprogrammer.kafka_chat_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaChatSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaChatSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobbyistprogrammer/kafka_chat_system/KafkaChatSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyistprogrammer.kafka_chat_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaChatSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
