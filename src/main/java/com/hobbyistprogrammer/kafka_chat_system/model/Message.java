c5c15c68fd519119b18772d813b329e3e2e3c928
hobbyist-programmer-ar <hobbyistprogrammer@outlook.com>
2025-06-07 23:22:00 +0530

    Adding the Model Message

---
+++ src/main/java/com/hobbyistprogrammer/kafka_chat_system/model/Message.java
@@ -0,0 +1,16 @@
+package com.hobbyistprogrammer.kafka_chat_system.model;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Message {
+    @Id
+    Long id;
+    String message;
+    LocalDateTime createdAt;
+    Boolean isDelivered;
+}
